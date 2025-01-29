1782eb8a86d9f843382a01e0619e652b88e4fd25
Max <21348106@stu.mmu.ac.uk>
2025-01-29 13:30:12 +0000

    build folder

---
+++ out/build/x64-Debug/launch-Lab05_Transformations.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/21348106/OneDrive - MMU/Documents/GitHub/Computer-Graphics-Labs/Lab05_Transformations/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\21348106\OneDrive - MMU\Documents\GitHub\Computer-Graphics-Labs\out\build\x64-Debug\Lab05_Transformations.exe"  
+pause
